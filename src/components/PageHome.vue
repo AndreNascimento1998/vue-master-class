9341b335a0703fbd6b5296a06445809e479ecbc4
Andre <andre.ncardoso@hotmail.com>
2023-09-20 18:01:26 -0300

    feat: new component

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,40 +1,15 @@
 <template>
-    <div class="container">
-        <span>Hellow from page home</span>
-        <div
-            v-for="thread in threads"
-            :key="threads.id"
-        >
-            <h2>{{ thread.title }}</h2>
-            
-            <div
-                v-for="postId in thread.posts"
-                :key="postId"
-            >
-                <p>{{ userById(postById(postId).userId).name }}</p>
-                <p>{{ postById(postId).text }}</p>
-            </div>
-        </div>
-    </div>
+    <h1>Welcome to the forum</h1>
+    <thread-list :threads="threads" />
 </template>
 
 <script setup>
 import sourceData from '@/data.json'
+import ThreadList from './ThreadList.vue';
 import { reactive } from 'vue';
 
-const {
-    threads,
-    posts,
-    users
-    } = reactive(sourceData)
 
-    function postById(postId) {
-        return posts.find(p => p.id === postId)
-    }
-
-    function userById(userId) {
-        return users.find(u => u.id === userId)
-    }
+const { threads } = reactive(sourceData)
 
 </script>
 
---
+++ src/components/ThreadList.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="col-full">
+
+        <div class="thread-list">
+
+            <h2 class="list-title">Threads</h2>
+
+            <div v-for="thread in threads" :key="threads.id" class="thread">
+                <div>
+                    <p>
+                        <a href="#">{{ thread.title }}</a>
+                    </p>
+                    <p class="text-faded text-xsmall">
+                        By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publisedhAt }}
+                    </p>
+                </div>
+
+                <div class="activity">
+                    <p class="replies-count">
+                        {{ thread.posts.length }}
+                    </p>
+
+                    <img class="avatar-medium" :src="userById(thread.userId).avatar" alt="">
+
+                    <div>
+                        <p class="text-xsmall">
+                            <a href="profile.html">{{ userById(thread.userId).name }}</a>
+                        </p>
+                        <p class="text-xsmall text-faded"> {{ thread.publisedhAt }}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import sourceData from '@/data.json'
+import { reactive } from 'vue';
+
+const props = defineProps({
+    threads: {
+        required: true
+    }
+})
+
+const {
+    posts,
+    users
+} = reactive(sourceData)
+
+function postById(postId) {
+    return posts.find(p => p.id === postId)
+}
+
+function userById(userId) {
+    return users.find(u => u.id === userId)
+}
+
+</script>
+
+<style scoped></style>
